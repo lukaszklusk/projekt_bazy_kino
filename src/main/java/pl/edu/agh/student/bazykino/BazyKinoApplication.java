7864c84cc81a316e175b9c9448debf4c63e729d2
Łukasz Kluska <lukaszklusk@gmail.com>
2023-05-24 00:12:00 +0200

    Spring boot setup

---
+++ src/main/java/pl/edu/agh/student/bazykino/BazyKinoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.student.bazykino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazyKinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazyKinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/student/bazykino/BazyKinoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.student.bazykino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazyKinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
